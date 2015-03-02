c8511c3a212f9cc6471813d18d03cf735d9730d0
Mike Linford <Mikey-spike@hotmail.co.uk>
2015-03-02 11:04:13 +0000

    Added Sliders
    Incorporated sliders into the main page.

--- www/js/index.js
+++ www/js/index.js
@@ -1,1 +1,20 @@
 $('.button-collapse').sideNav({menuWidth: 240, activationWidth: 100});
+
+var swiper = new Swiper('#swiper-1', {
+        pagination: '#swiper-pagination-1',
+        paginationClickable: true,
+        loop: true,
+        slidesPerView: 1
+    });
+var swiper = new Swiper('#swiper-2', {
+        pagination: '#swiper-pagination-2',
+        paginationClickable: true,
+        loop: true,
+        slidesPerView: 1
+    });
+var swiper = new Swiper('#swiper-3', {
+        pagination: '#swiper-pagination-3',
+        paginationClickable: true,
+        loop: true,
+        slidesPerView: 1
+    });
